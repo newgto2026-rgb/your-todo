ca8911da97d35b024843647052f2c60a5c89a695
tee <newgto2001@naver.com>
2026-05-16 14:14:30 +0900

    Address AI sheet inset review

--- feature/todo/impl/src/main/java/com/neo/yourtodo/feature/todo/impl/ui/screen/TodoListScreen.kt
+++ feature/todo/impl/src/main/java/com/neo/yourtodo/feature/todo/impl/ui/screen/TodoListScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -524,7 +525,6 @@ private fun LockedAiBottomSheetDialog(
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .imePadding()
                 .testTag("ai_todo_dialog")
         ) {
             Box(
@@ -558,6 +558,8 @@ private fun LockedAiBottomSheetDialog(
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
                     .fillMaxWidth()
+                    .imePadding()
+                    .navigationBarsPadding()
                     .testTag("ai_todo_sheet_container")
             ) {
                 content()
